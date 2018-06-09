f064d4ee15b3f3f95012e0ecbbda8119d082b6a6
Darek Grund <darek.grund@gmail.com>
2018-06-09 10:41:21 +0200

    Vuex demo

--- app/main.js
+++ app/main.js
@@ -4,30 +4,97 @@ const state = {
 };
 
 const mutations = {
-    ADD_NOTE () {
-
+    ADD_NOTE (state, payload) {
+        let newNote = payload;
+        state.notes.push(newNote);
     },
 
-    ADD_TIMESTAMP () {
-
+    ADD_TIMESTAMP (state, payload) {
+        let newTimeStamp = payload;
+        state.timestamps.push(newTimeStamp);
     },
+};
+
+const actions = {
+    addNote (context, payload) {
+        context.commit('ADD_NOTE', payload)
+    },
+
+    addTimestamp (context, payload) {
+        context.commit('ADD_TIMESTAMP', payload)
+    },
+};
+
+// getters are optional,
+// because we can directly address
+//      this.$store.state
+// instead of
+//      this.$store.getters
+const getters = {
+    getNotes: state => state.notes,
+    getTimestamps: state => state.timestamps,
+    getNoteCount: state => state.timestamps.length,
 };
 
 const inputComponent = {
     template: `<input placeholder='Enter a note' class="input is-small" type="text"
-                @keyup.emter="onKeyEnter()" 
+                v-model="input"
+                @keyup.enter="onKeyEnter()"
                 />`,
+
+    data () {
+        return {
+            input: '',
+        }
+    },
 
     methods: {
         onKeyEnter () {
-            alert(1)
+            this.$store.dispatch('addNote', this.input);
+            store.dispatch('addTimestamp', new Date().toLocaleDateString());
+            this.input = '';
         }
     },
 };
 
+const noteCountComponent = {
+    template:
+        `<div class="note-count">Note count: <strong>{{ noteCount }}</strong></div>`,
+    computed: {
+        noteCount() {
+            return this.$store.getters.getNoteCount;
+        }
+    }
+};
+
+const store = new Vuex.Store({
+    state,
+    mutations,
+    actions,
+    getters,
+});
+
 new Vue({
     el: '#app',
+    store,
     components: {
-        'input-component': inputComponent
+        'input-component': inputComponent,
+        'note-count-component': noteCountComponent
     },
+
+    computed: {
+        notes () {
+            return this.$store.getters.getNotes;
+        },
+
+        timestamps () {
+            return this.$store.state.timestamps;
+        }
+    },
+
+    mounted () {
+
+    }
 });
+
+
